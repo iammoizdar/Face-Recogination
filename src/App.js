62063cdf1da17337fcd6fb2b107d3384e6c85ad5
Abdul Moiz <moiz.webevis@gmail.com>
2022-03-08 17:58:25 +0500

    moremore grind

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-import { useState } from 'react';
-import './App.css';
-import Caruploader from './components/Carduploader';
-import WebcamComp from './components/WebcamComp';
-import swal from 'sweetalert';
+import { useState } from "react";
+import "./App.css";
+import Caruploader from "./components/Carduploader";
+import WebcamComp from "./components/WebcamComp";
+import swal from "sweetalert";
 function App() {
-const [data,setData]=useState({card_image:'',selfie:''})
+  const [data, setData] = useState({ card_image: "", selfie: "" });
 
+  const runfunction = () => {
+    var axios = require("axios");
+    console.log(data);
+    var dataresult = JSON.stringify(data);
+    console.log(dataresult);
+    // const data={
+    //   card_img:'base64card',
+    //   camera_img:'base64'
+    // }
 
-const runfunction =()=>{
-var axios = require('axios');
-console.log(data)
-var dataresult = JSON.stringify(data);
-console.log(dataresult)
-// const data={
-//   card_img:'base64card',
-//   camera_img:'base64'
-// }
+    var config = {
+      method: "post",
+      url: "http://192.168.0.103:5000/verification",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: dataresult,
+    };
 
-var config = {
-method: 'post',
-url: 'http://192.168.100.198:5000/verification',
-headers: {
-'Content-Type': 'application/json'
-},
-data : dataresult
-};
-
-axios(config)
-.then(function (response) {
-console.log(JSON.stringify(response.data));
-response.data.is_verified ? swal("Match Found!","Wanna Try Again!", "success") : swal("Match Not Found", "Try Again!", "error")
-.catch(function (error) {
-console.log(error);
-});
-}
-
-
-
-)}
+    axios(config).then(function (response) {
+      console.log(JSON.stringify(response.data));
+      response.data.is_verified
+        ? swal("Match Found!", "Wanna Try Again!", "success")
+        : swal("Match Not Found", "Try Again!", "error").catch(function (
+            error
+          ) {
+            console.log(error);
+          });
+    });
+  };
 
   return (
     <div className="App">
-    <Caruploader setData={setData}/>
-    <WebcamComp setData={setData}/>
-    <button className='button1' onClick={runfunction}>Send</button>
+      <Caruploader setData={setData} />
+      <WebcamComp setData={setData} />
+      <button className="button1" onClick={runfunction}>
+        Send
+      </button>
     </div>
   );
 }
